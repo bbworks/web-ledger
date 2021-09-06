97b8b7c8de23a3603a0743c11ed94ce837d8e4b9
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2021-09-06 14:24:08 -0400

    Renamed utilities/dashbaord.js to utilities/budgetCycle.js
    - renamed utilities/dashbaord.js to utilities/budgetCycle.js

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,6 +2,6 @@ export * from './transactions.js';
 export * from './budgetsData.js';
 export * from './accountsData.js';
 export * from './accountData.js';
-export * from './dashboard.js';
+export * from './budgetCycle.js';
 export * from './utilities.js';
 export * from './errorHandling.js';
