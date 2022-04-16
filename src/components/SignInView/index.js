148e3d5bcdaaf5d5efddded8a4a89608d5b0b210
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2022-04-15 21:59:35 -0400

    SignInView bugfix
    - bugfix  where the view was longer than the viewheight

--- src/components/SignInView/index.js
+++ src/components/SignInView/index.js
@@ -6,7 +6,7 @@ import './index.scss';
 const SignInView = ({ isReadyForSignIn })=>{
   return (
     <div className="view signin-view container-fluid">
-      <div className="container-fluid d-flex flex-column justify-content-center align-items-center min-vh-100">
+      <div className="signin-view-container container-fluid d-flex flex-column justify-content-center align-items-center">
         <Logo />
         <GoogleApiSignInButton isReadyForSignIn={isReadyForSignIn} />
       </div>
