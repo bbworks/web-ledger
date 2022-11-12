106916b88b5920ad52f887f885777757bde01604
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2022-11-12 15:32:00 -0500

    Disabling old Google API authentication code
    - commenting out old code for React compiler

--- client/src/googleApi/authentication.js
+++ client/src/googleApi/authentication.js
@@ -1,137 +1,137 @@
-import {throwException} from './../utilities';
-
-const scopes = [
-  //"https://www.googleapis.com/auth/drive",  //See, edit, create, and delete all or your drive files
-  //"https://www.googleapis.com/auth/drive.file",  //See, edit, create, and delete only the specific Google Drive files you use with this app
-  "https://www.googleapis.com/auth/spreadsheets",  //See, edit, create, all your Google sheets spreadsheets
-];
-const discoveryDocs = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-
-//create helper function to retrieve authorization credentials stored outside of version control
-const fetchAuthCredentials = ()=>{
-  console.info("Fetching authorization credentials...");
-
-  try {
-    //Get & return the credentials from localStorage
-    const creds = JSON.parse(localStorage.getItem("creds"));
-    if (creds && creds.clientId) return creds;
-
-    //If proper credentials weren't found, throw
-    const CredentialsNotFoundError = new Error("Failed to get authorization credentials.")
-    CredentialsNotFoundError.name = "CredentialsNotFoundError";
-    throw CredentialsNotFoundError;
-  }
-  catch (err) {
-    throw err;
-  }
-};
-
-//Load the Google Client, OAuth2.0 libraries
-export const initAuthorization = (loginCallback, logoutCallback)=>{
-  try {
-    //Attempt to get the credentials
-    const creds = fetchAuthCredentials();
-
-    //Load the Google APIs
-    loadGoogleApis(creds, loginCallback, logoutCallback)
-
-    return creds;
-  }
-  catch (err) {
-    return throwException(err);
-  }
-};
-
-const loadGoogleApis = (creds, loginCallback, logoutCallback)=>{
-  try {
-    console.info("Loading Google Client, OAuth2.0 APIs...");
-    window.gapi.load("client:auth2", ()=>{initializeGoogleApis(creds, loginCallback, logoutCallback)});
-  }
-  catch (err) {
-    return throwException(err);
-  }
-};
-
-//Initialize Google Client library (which simultaneously initializes Google OAuth2.0 library) and set up sign in listeners
-const initializeGoogleApis = async (creds, loginCallback, logoutCallback)=>{
-  console.info("Loaded Google Client, OAuth2.0 APIs.");
-  console.info("Initializing Google Client API...");
-  const {clientId} = creds;
-
-  //Attempt to initialize the Google API
-  window.gapi.client.init({
-    clientId: clientId,
-    scope: scopes.join(" "), //space delimited
-    discoveryDocs: discoveryDocs,
-  })
-  .then(()=>{
-    console.info("Initialized Google Client API.");
-
-    //Listen for sign in state changes
-    window.gapi.auth2.getAuthInstance().isSignedIn.listen(
-      isSignedIn => updateSigninStatus(isSignedIn, loginCallback, logoutCallback)
-    );
-
-    //Handle initial sign in state
-    updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get(), loginCallback, logoutCallback);
-  })
-  .catch(err=>{
-    return throwException(err);
-  });
-};
-
-//Update UI sign in state changes
-const updateSigninStatus = (isSignedIn, loginCallback, logoutCallback)=>{
-  console.info("Updating sign in status...");
-  if (isSignedIn) {
-    // authorizeButton.style.display = "none";
-    // signoutButton.style.display = "block";
-    // signoutButton.removeAttribute("disabled");
-
-    //Get profile information
-    const signInInfo = getProfileInformation();
-
-    //Run the callback functions
-    if (loginCallback) loginCallback(signInInfo);
-  }
-  else {
-    // authorizeButton.style.display = "block";
-    // signoutButton.style.display = "none";
-
-    //Run the callback functions
-    if (logoutCallback) logoutCallback(null);
-  }
-  console.info("Updated sign in status.");
-};
-
-export const signIn = async ()=>{
-  try {
-    await window.gapi.auth2.getAuthInstance().signIn();
-  }
-  catch (err) {
-    return throwException(err, false, false);
-  }
-};
-
-export const signOut = async ()=>{
-  try {
-    await window.gapi.auth2.getAuthInstance().signOut();
-  }
-  catch (err) {
-    return throwException(err, false, false);
-  }
-};
-
-const getProfileInformation = ()=>{
-  console.info("Getting profile info...");
-  const profile = window.gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
-  if (!profile) return null;
-  return {
-    id: profile.getId(),
-    name: profile.getName(),
-    firstName: profile.getGivenName(),
-    lastName: profile.getFamilyName(),
-    imageUrl: profile.getImageUrl(),
-    emailAddress: profile.getEmail(),
-  };
-};
+// import {throwException} from './../utilities';
+//
+// const scopes = [
+//   //"https://www.googleapis.com/auth/drive",  //See, edit, create, and delete all or your drive files
+//   //"https://www.googleapis.com/auth/drive.file",  //See, edit, create, and delete only the specific Google Drive files you use with this app
+//   "https://www.googleapis.com/auth/spreadsheets",  //See, edit, create, all your Google sheets spreadsheets
+// ];
+// const discoveryDocs = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+//
+// //create helper function to retrieve authorization credentials stored outside of version control
+// const fetchAuthCredentials = ()=>{
+//   console.info("Fetching authorization credentials...");
+//
+//   try {
+//     //Get & return the credentials from localStorage
+//     const creds = JSON.parse(localStorage.getItem("creds"));
+//     if (creds && creds.clientId) return creds;
+//
+//     //If proper credentials weren't found, throw
+//     const CredentialsNotFoundError = new Error("Failed to get authorization credentials.")
+//     CredentialsNotFoundError.name = "CredentialsNotFoundError";
+//     throw CredentialsNotFoundError;
+//   }
+//   catch (err) {
+//     throw err;
+//   }
+// };
+//
+// //Load the Google Client, OAuth2.0 libraries
+// export const initAuthorization = (loginCallback, logoutCallback)=>{
+//   try {
+//     //Attempt to get the credentials
+//     const creds = fetchAuthCredentials();
+//
+//     //Load the Google APIs
+//     loadGoogleApis(creds, loginCallback, logoutCallback)
+//
+//     return creds;
+//   }
+//   catch (err) {
+//     return throwException(err);
+//   }
+// };
+//
+// const loadGoogleApis = (creds, loginCallback, logoutCallback)=>{
+//   try {
+//     console.info("Loading Google Client, OAuth2.0 APIs...");
+//     window.gapi.load("client:auth2", ()=>{initializeGoogleApis(creds, loginCallback, logoutCallback)});
+//   }
+//   catch (err) {
+//     return throwException(err);
+//   }
+// };
+//
+// //Initialize Google Client library (which simultaneously initializes Google OAuth2.0 library) and set up sign in listeners
+// const initializeGoogleApis = async (creds, loginCallback, logoutCallback)=>{
+//   console.info("Loaded Google Client, OAuth2.0 APIs.");
+//   console.info("Initializing Google Client API...");
+//   const {clientId} = creds;
+//
+//   //Attempt to initialize the Google API
+//   window.gapi.client.init({
+//     clientId: clientId,
+//     scope: scopes.join(" "), //space delimited
+//     discoveryDocs: discoveryDocs,
+//   })
+//   .then(()=>{
+//     console.info("Initialized Google Client API.");
+//
+//     //Listen for sign in state changes
+//     window.gapi.auth2.getAuthInstance().isSignedIn.listen(
+//       isSignedIn => updateSigninStatus(isSignedIn, loginCallback, logoutCallback)
+//     );
+//
+//     //Handle initial sign in state
+//     updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get(), loginCallback, logoutCallback);
+//   })
+//   .catch(err=>{
+//     return throwException(err);
+//   });
+// };
+//
+// //Update UI sign in state changes
+// const updateSigninStatus = (isSignedIn, loginCallback, logoutCallback)=>{
+//   console.info("Updating sign in status...");
+//   if (isSignedIn) {
+//     // authorizeButton.style.display = "none";
+//     // signoutButton.style.display = "block";
+//     // signoutButton.removeAttribute("disabled");
+//
+//     //Get profile information
+//     const signInInfo = getProfileInformation();
+//
+//     //Run the callback functions
+//     if (loginCallback) loginCallback(signInInfo);
+//   }
+//   else {
+//     // authorizeButton.style.display = "block";
+//     // signoutButton.style.display = "none";
+//
+//     //Run the callback functions
+//     if (logoutCallback) logoutCallback(null);
+//   }
+//   console.info("Updated sign in status.");
+// };
+//
+// export const signIn = async ()=>{
+//   try {
+//     await window.gapi.auth2.getAuthInstance().signIn();
+//   }
+//   catch (err) {
+//     return throwException(err, false, false);
+//   }
+// };
+//
+// export const signOut = async ()=>{
+//   try {
+//     await window.gapi.auth2.getAuthInstance().signOut();
+//   }
+//   catch (err) {
+//     return throwException(err, false, false);
+//   }
+// };
+//
+// const getProfileInformation = ()=>{
+//   console.info("Getting profile info...");
+//   const profile = window.gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
+//   if (!profile) return null;
+//   return {
+//     id: profile.getId(),
+//     name: profile.getName(),
+//     firstName: profile.getGivenName(),
+//     lastName: profile.getFamilyName(),
+//     imageUrl: profile.getImageUrl(),
+//     emailAddress: profile.getEmail(),
+//   };
+// };
