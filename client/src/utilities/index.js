9c3228c076c4ec6beeabea7a8f1c0ea4a31b165a
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2022-11-21 18:48:28 -0500

    Removal of errorHandling utility module
    - removed unnecessary errorHandling utility functions module

--- client/src/utilities/index.js
+++ client/src/utilities/index.js
@@ -4,5 +4,4 @@ export * from './accountsData.js';
 export * from './accountData.js';
 export * from './budgetCycle.js';
 export * from './utilities.js';
-export * from './errorHandling.js';
 export * from './date.js';
