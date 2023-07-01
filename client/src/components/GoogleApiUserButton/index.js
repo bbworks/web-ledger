ff3f0455593ecedc59e712400287d341267f2a29
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2023-07-01 19:04:05 -0400

    GoogleApiUserButton display bugfix
    - fixed GoogleApiUserButton incorrect property name displays (`firstName`->`given_name`, `imageUrl`->`picture`)

--- client/src/components/GoogleApiUserButton/index.js
+++ client/src/components/GoogleApiUserButton/index.js
@@ -4,8 +4,8 @@ const GoogleApiUserButton = ({ signedInUser })=>{
   return (
     <div className="dropdown">
       <button className="user-button btn bg-light" type="button" id="user-button" data-bs-toggle="dropdown" aria-expanded="false">
-        <img className="user-button-image" src={signedInUser.imageUrl} alt="signed in user image"/>
-        <span className="user-button-name">{signedInUser.firstName}</span>
+        <img className="user-button-image" src={signedInUser.picture} alt="signed in user image"/>
+        <span className="user-button-name">{signedInUser.given_name}</span>
       </button>
       <ul className="dropdown-menu" aria-labelledby="user-button">
         <li><a className="dropdown-item" onClick={/* signOut */()=>{}}>Sign out</a></li>
