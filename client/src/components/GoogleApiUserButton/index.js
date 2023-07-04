a73534aeaf385237b81cff69fa19f5c5a3de477b
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2023-07-04 10:39:37 -0400

    Dummy onLogOut for GoogleApiUserButton
    - passing down dummy onLogOut prop to GoogleApiUserButton (to remove actual Google Client API)

--- client/src/components/GoogleApiUserButton/index.js
+++ client/src/components/GoogleApiUserButton/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 
-const GoogleApiUserButton = ({ signedInUser })=>{
+const GoogleApiUserButton = ({ signedInUser, onLogOut })=>{
   return (
     <div className="dropdown">
       <button className="user-button btn bg-light" type="button" id="user-button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -8,7 +8,7 @@ const GoogleApiUserButton = ({ signedInUser })=>{
         <span className="user-button-name">{signedInUser.given_name}</span>
       </button>
       <ul className="dropdown-menu" aria-labelledby="user-button">
-        <li><a className="dropdown-item" onClick={/* signOut */()=>{}}>Sign out</a></li>
+        <li><a className="dropdown-item" href="#" onClick={onLogOut}>Sign out</a></li>
       </ul>
     </div>
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,13 +3,13 @@ import GoogleApiUserButton from './../GoogleApiUserButton';
 
 import './index.scss';
 
-const Header = ({ signedInUser })=>{
+const Header = ({ signedInUser, onLogOut })=>{
   return (
     <header className="dashboard-header navbar-text fixed-top bg-primary">
       <div className="container d-flex justify-content-between">
         <Logo light />
         <div className="d-flex justify-content-center align-items-center">
-          <GoogleApiUserButton signedInUser={signedInUser}/>
+          <GoogleApiUserButton signedInUser={signedInUser} onLogOut={onLogOut} />
         </div>
       </div>
     </header>
